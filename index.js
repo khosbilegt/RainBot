17dbe1d7db2335173aef587534e03fa0574ab506
khosbilegt <khosookoo@gmail.com>
2022-12-03 15:27:16 +0800

    added welcome

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const Discord = require('discord.js')
 const {Client, GatewayIntentBits} = require('discord.js')
 require("dotenv").config()
 
+// Custom Imports
+const generateImage = require('./Welcome/imageGenerator')
+
+
 const client = new Discord.Client({
      intents: [
           GatewayIntentBits.Guilds,
-          GatewayIntentBits.GuildMessages
+          GatewayIntentBits.GuildMessages,
+          GatewayIntentBits.GuildMembers
      ]
 })
 
@@ -13,10 +18,14 @@ client.on("ready", () => {
      console.log(`Logged in as ${client.user.tag}`)
 })
 
-client.on("messageCreate", (message) => {
-     if(message.content = "hi") {
-          message.reply("Hello World!")
-     }
+const welcomeChannel = "1048462304297111563"
+
+client.on("guildMemberAdd", async (member) => {
+     const img = await generateImage(member)
+     member.guild.channels.cache.get(welcomeChannel).send({
+          content: `<@${member.id}> Welcome to the server!`,
+          files: [img]
+     })
 })
 
 client.login(process.env.TOKEN)
